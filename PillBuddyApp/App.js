d4ec69bc62239a4bb9256d304b61578f2a81e23c
Mohammed Rahman <m2rahman@ucsd.edu>
2019-05-23 17:20:57 -0700

    Added tab bar navigation

--- PillBuddyApp/App.js
+++ PillBuddyApp/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import AppNavigator from './AppNavigator';
+import TabTest from './TabTest';
 
 export default class App extends React.Component {
   render() {
     return (
-      <AppNavigator/>
+      <TabTest/>
     );
   }
 }
---
+++ PillBuddyApp/TabTest.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import { AppRegistry, TabBarIOS, StyleSheet, Text, View, Button} from 'react-native';
+import HomePage from './components/HomePage/HomePage';
+import Icon from 'react-native-vector-icons/FontAwesome'; // use for tab icons
+
+export default class StarterTabBarIOS extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {selectedTab: 'tabHistory'};
+  }
+
+  setTab(tabId) {
+    this.setState({selectedTab: tabId});
+  }
+
+  render() {
+    return (
+      <TabBarIOS>
+        <TabBarIOS.Item
+          title={ 'Dosage History' }
+          selected={this.state.selectedTab === 'tabHistory'}
+          onPress={() => this.setTab('tabHistory')}>
+          <TabHistory/>
+        </TabBarIOS.Item>
+
+        <TabBarIOS.Item
+          title={ 'Manage Pills' }
+          selected={this.state.selectedTab === 'tabPills'}
+          onPress={() => this.setTab('tabPills')}>
+          <TabPills/>
+        </TabBarIOS.Item>
+
+        <TabBarIOS.Item
+          title={ 'Settings' }
+          selected={this.state.selectedTab === 'tabSettings'}
+          onPress={() => this.setTab('tabSettings')}>
+          <TabSettings/>
+        </TabBarIOS.Item>
+      </TabBarIOS>
+    );
+  }
+
+}
+
+/**
+ * History Tab
+ */
+class TabHistory extends React.Component {
+
+  render() {
+    return (
+      <View style={styles.tabContent}>
+        <Text style={styles.tabText}>Dosage History</Text>
+      </View>
+    );
+  }
+
+}
+
+/**
+ * Pills Tab
+ */
+class TabPills extends React.Component {
+
+  render() {
+    return (
+        <HomePage/>
+    );
+  }
+
+}
+
+/**
+ * Settings Tab
+ */
+class TabSettings extends React.Component {
+
+  render() {
+    return (
+      <View style={styles.tabContent}>
+        <Text style={styles.tabText}>Settings</Text>
+      </View>
+    );
+  }
+
+}
+
+var styles = StyleSheet.create({
+  tabContent: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  tabText: {
+    margin: 50,
+    fontSize: 40
+  }
+});
+
+AppRegistry.registerComponent('StarterTabBarIOS', () => StarterTabBarIOS);
