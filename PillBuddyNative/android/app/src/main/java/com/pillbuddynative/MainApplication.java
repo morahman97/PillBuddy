ff15cf66e8f5c34359b094f5096072b97c24c9dc
Shaheryar Ajmal <sajmal@ucsd.edu>
2019-06-03 17:36:05 -0700

    Added RNSVG to podfile

--- PillBuddyNative/android/app/src/main/java/com/pillbuddynative/MainApplication.java
+++ PillBuddyNative/android/app/src/main/java/com/pillbuddynative/MainApplication.java
@@ -3,6 +3,7 @@ package com.pillbuddynative;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import it.innove.BleManagerPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SvgPackage(),
             new RNFirebasePackage(),
             new RNGestureHandlerPackage(),
             new BleManagerPackage()
